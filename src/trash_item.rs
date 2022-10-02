a20909af9b2e0f7f8c8eea20fde494ac5dcd7504
AmineZouitine <mouldor.lol@gmail.com>
2022-10-02 22:38:46 +0200

    implementation displat for trash items

--- src/trash_item.rs
+++ src/trash_item.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 #[derive(Debug, PartialEq)]
 pub struct TrashItem {
     pub id: i8,
@@ -27,5 +29,18 @@ impl TrashItem {
             real_size,
             compression_size,
         }
+    }
+}
+
+
+impl fmt::Display for TrashItem
+{
+     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let mut compression_size = 0;
+        if let Some(size) = self.compression_size {
+            compression_size = size;
+        }
+
+        write!(f, " date: {}  name: {}  inital_path: {} size: {} compressed: {} compression_size: {}", self.date, self.name, self.path,self.real_size, self.compression_size.is_some(), compression_size)
     }
 }
