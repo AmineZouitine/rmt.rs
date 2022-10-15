0da23e3aeb895efaa208406f8f86d0f4d955fe65
AmineZouitine <mouldor.lol@gmail.com>
2022-10-15 06:19:04 +0200

    save integration_test

--- tests/arguments_tests.rs
+++ tests/arguments_tests.rs
@@ -1,12 +1,15 @@
 use assert_cmd::prelude::*;
+use colored::ColoredString;
 use predicates::prelude::*;
 use rmt_lib::argument_errors::{RmtArgumentErrors};
 use std::{process::Command, os::unix::process::CommandExt};
+use colored::*;
 
 #[test]
 fn test_no_args() {
     let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
-    let expected_output = RmtArgumentErrors::InvalidNumberOfArguments(0).to_string();
+    // let expected_output = RmtArgumentErrors::InvalidNumberOfArguments(0).to_string();
+    println!("{}", ColoredString::
     // cmd.assert()
     //     .failure()
     //     .stdout(predicate::str::diff(expected_output));
