32048b672232804375fc00f849a02113b843c08a
AmineZouitine <mouldor.lol@gmail.com>
2022-10-15 11:15:44 +0200

    add integration test for folders

--- tests/arguments_tests.rs
+++ tests/arguments_tests.rs
@@ -1,7 +1,17 @@
 use assert_cmd::prelude::*;
+use core::time;
 use predicates::prelude::*;
 use rmt_lib::argument_errors::RmtArgumentErrors;
-use std::process::Command;
+use std::{fs, process::Command};
+
+fn unique_name() -> String {
+    std::thread::sleep(time::Duration::from_nanos(1));
+    sha256::digest(format!(
+        "{}",
+        chrono::offset::Local::now().timestamp_nanos()
+    ))
+}
+
 #[test]
 fn test_no_args() {
     let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
@@ -28,11 +38,7 @@ fn test_only_flags() {
 fn test_not_existing_file_without_force() {
     let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
 
-    // I hope you don't have a file name like that
-    let not_existing_file_name = sha256::digest(format!(
-        "{}",
-        chrono::offset::Local::now().timestamp_nanos()
-    ));
+    let not_existing_file_name = unique_name();
 
     cmd.arg(&not_existing_file_name);
 
@@ -51,9 +57,34 @@ fn test_not_existing_file_without_force() {
 fn test_not_existing_file_with_force() {
     let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
 
-    let not_existing_file_name = "rmt_integration_test_not_existing_file.rmt".to_string();
-    // I hope you don't have a file name like that
+    let not_existing_file_name = unique_name();
+
     cmd.arg(&not_existing_file_name).arg("-f");
 
-    cmd.assert().success().stdout(predicate::str::diff(""));
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::diff(""));
+}
+
+#[test]
+fn test_delele_empty_folder_without_flags() {
+    let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+
+    let not_existing_folder_name = unique_name();
+
+    fs::create_dir_all(&not_existing_folder_name).unwrap();
+
+    cmd.arg(&not_existing_folder_name);
+
+    let expected_output = format!(
+        "{}\n",
+        RmtArgumentErrors::InvalidEmptyFolderFlags { 
+            folder_name: format!("{}/{}", std::env::current_dir().unwrap().display(), not_existing_folder_name)
+        }
+    );
+    cmd.assert()
+        .failure()
+        .stdout(predicate::str::diff(expected_output));
+
+    fs::remove_dir_all(&not_existing_folder_name).unwrap();
 }
