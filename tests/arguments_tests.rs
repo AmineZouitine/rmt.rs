ad2a8cea2f8affc06a3246b80dc32432f2e4b936
AmineZouitine <mouldor.lol@gmail.com>
2022-10-15 07:55:52 +0200

    add test not existing fill with/without force flags

--- tests/arguments_tests.rs
+++ tests/arguments_tests.rs
@@ -42,3 +42,21 @@ fn test_not_existing_file_without_force() {
         .failure()
         .stdout(predicate::str::diff(expected_output));
 }
+
+
+#[test]
+fn test_not_existing_file_with_force() {
+    let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+
+    let not_existing_file_name = "rmt_not_existing_file.rmt".to_string();
+    // I hope you don't have a file name like that
+    cmd.arg(&not_existing_file_name).arg("-f");
+
+    let expected_output = format!(
+        "{}\n",
+        RmtArgumentErrors::InvalidPathWithForceFlags
+    );
+    cmd.assert()
+        .failure()
+        .stdout(predicate::str::diff(expected_output));
+}
